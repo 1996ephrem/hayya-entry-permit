200e7825d25ea0417f31f14a69b3e9cbb07016bb
ephrem shitu <ephremshitu14@gmail.com>
2026-08-14 04:50:21 -0700

    Configure Firebase with project credentials

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,14 @@
 // Get them from: https://console.firebase.google.com
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyD8RenzWLJZF3BipubiURZ7Td4Ppgy4D2o",
+  authDomain: "hayya-entry-permit.firebaseapp.com",
+  databaseURL: "https://hayya-entry-permit-default-rtdb.firebaseio.com",
+  projectId: "hayya-entry-permit",
+  storageBucket: "hayya-entry-permit.firebasestorage.app",
+  messagingSenderId: "510676565272",
+  appId: "1:510676565272:web:2420666930648b57a3b26d",
+  measurementId: "G-43L3LRT1RR"
 };
 
 // Initialize Firebase
